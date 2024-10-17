2de5190497ef7e0e3f325af3376b92d2d0af03ce
jpcarney <jp_carney@dlsu.edu.ph>
2024-10-17 21:34:54 +0800

    Initial commit, tested dash library

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='STADVDB-MCO1',
+    version='1.0',
+    packages=['MCO1'],
+    url='https://github.com/jpcarney/STADVDB-MCO1',
+    license='',
+    author='John Paul C. Carney',
+    author_email='jpcarney@dlsu.edu.ph',
+    description='OLAP Web Application for STADVDB MCO1 Output',
+    install_requires = ['dash'],
+)
